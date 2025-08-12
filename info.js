b0a2960428fd39c81230e13fecb7043425d8b7c8
offici5l <127665988+offici5l@users.noreply.github.com>
2025-08-12 06:41:37 +0100

    Update info.js

--- info.js
+++ info.js
@@ -3,7 +3,8 @@ const userInfo = {
     username_x: 'offici5l',
     username_telegram: 'Offici5l_Channel',
     email: 'offici5lcontact@proton.me',
-    homepage: 'https://offici5l.github.io',
+    github_topic: 'offici5l',
+    
     support_buttons: {
         proton_wallet: {
             name: 'with Proton Wallet',
@@ -15,5 +16,8 @@ const userInfo = {
             redirect: 'https://pay.oxapay.com/19904987',
             svg: 'https://oxapay.com/_next/static/media/wallet.dbfb0bff.svg'
         }
-    }
+    },
+    about_me: `Passionate about technology in general, I believe that true innovation lies in ideas that transcend the ordinary and create positive transformation. I began my journey in the world of programming in 2023, facing significant challenges due to limited access to a modern computer—I have an old and nearly broken laptop. However, I turned this constraint into a unique opportunity, learning and developing most of my projects (about 98%) entirely using only my mobile phone. This persistence inspired me to create custom solutions for those who face similar challenges in accessing a laptop.
+
+I adopt a flexible approach to learning, currently benefiting particularly from advancements in artificial intelligence to save time and focus on generating new ideas. I always see myself on the path of continuous learning.`
 };
