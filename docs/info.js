2a144d0a26b5cb3bba312c9e517bf77227428148
offici5l <offici5l@protonmail.com>
2025-09-24 17:19:52 +0100

    .

--- docs/info.js
+++ docs/info.js
@@ -4,7 +4,7 @@ const userInfo = {
     username_x: 'offici5l',
     username_telegram: 'Offici5l_Channel',
     email: 'offici5lcontact@proton.me',
-    projects_url: 'https://offici5l.github.io/projects.html',
+    projects_url: 'projects.html',
     base_url: 'https://offici5l.github.io/',
     support_buttons: {
         bitcoin: {
