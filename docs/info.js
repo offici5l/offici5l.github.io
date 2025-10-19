2c4fb6c2694f5c29a6d1166499aed4cd23332cb2
offici5l <offici5l@protonmail.com>
2025-10-19 12:47:55 +0100

    .

--- docs/info.js
+++ docs/info.js
@@ -12,6 +12,10 @@ const userInfo = {
             address: 'bc1qaqyaxz4t05wh434em3ss7q6dysfl7t348tmm08'
         },
         usdt: {
+            name: 'USDT (BEP20)',
+            address: '0xE9f184635C9ecD6b0B18E669aFCee1352E8982Ec'
+        }
+        usdt: {
             name: 'USDT (TRC-20)',
             address: 'TQAqYBMVMHrHzNWHMHYhP6MnVxeE84e7su'
         }
