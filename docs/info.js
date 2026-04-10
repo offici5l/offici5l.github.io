adeadfb0f3cd050964815073750fca0cea07e997
offici5l <offici5l@protonmail.com>
2026-04-10 22:26:23 +0100

    remove crypto donation section

--- docs/info.js
+++ docs/info.js
@@ -5,9 +5,5 @@ const INFO = {
   telegram: 'https://t.me/Offici5l_Channel',
   twitter: 'https://x.com/offici5l',
   email: 'offici5lcontact@proton.me',
-  crypto: [
-    { tag: 'BTC', cls: 'btc', address: 'bc1qaqyaxz4t05wh434em3ss7q6dysfl7t348tmm08' },
-    { tag: 'BEP20', cls: 'bep', address: '0xE9f184635C9ecD6b0B18E669aFCee1352E8982Ec' },
-    { tag: 'TRC20', cls: 'trc', address: 'TQAqYBMVMHrHzNWHMHYhP6MnVxeE84e7su' },
-  ]
+
 };
