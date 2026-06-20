4a20544e0da1e709496bd3648d78962842a38bc5
offici5l <127665988+offici5l@users.noreply.github.com>
2026-06-20 01:20:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   email: 'offici5lcontact@proton.me',
   logo: 'logo.png',
   site_name: 'offici5l',
-  site_desc: 'making stuff that works · open source',
+  site_desc: 'offici5l | Open source developer who started his journey in 2023, building and shipping everything from his phone only. No computer.',
   blog_url: false,
   cf_analytics: '1825761b3d024affb84ca8cb579990bf',
 };
